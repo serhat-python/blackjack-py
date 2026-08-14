f2ea3bf08c60d5f240aa0fd6e154b2dd09e16a82
Serhat Özdemir <serhat.oezd@web.de>
2026-08-14 04:58:27 +0200

    Funktionen hinzugefügt

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 import art
 import random
 
+def neustart():
+    global blackjack, game, count_comp, count_user, user, comp, choice
+    choice = input("Do u want to play again? Type 'y' or 'n'")
+    if choice == "n":
+        blackjack = False
+    elif choice == "y":
+        game = True
+        count_comp = 0
+        count_user = 0
+        user = []
+        comp = []
+    else:
+        blackjack = False
+
+
+def user_add():
+    global  count_user, user
+    user.append(random.choice(cards))
+    count_user = 0
+    for add in user:
+        count_user += add
+        if 11 in user and count_user > 21:
+            user[user.index(11)] = 1
+            count_user -= 10
+
+
+def comp_add():
+    global comp, count_comp
+    comp.append(random.choice(cards))
+    count_comp = 0
+    for add in comp:
+        count_comp += add
+    if 11 in comp and count_comp > 21:
+        comp[comp.index(11)] = 1
+        count_comp -= 10
+
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 choice = input("Do you want to play a game of blackjack? Type 'y' or 'n'")
@@ -31,48 +68,18 @@ while blackjack:
             else:
                 choice = input("Type 'y' to get another card, type 'n' to pass: ")
             if choice == "y":
-                user.append(random.choice(cards))
-                count_user = 0
-                for add in user:
-                    count_user += add
-                    if 11 in user and count_user > 21:
-                        user[user.index(11)] = 1
-                        count_user -= 10
+                user_add()
                 for add in comp:
                     count_comp += add
                 if count_user > 21:
                     print(f"Your final hand: {user}, current score: {count_user}")
                     print(f"Computer's final hand: {comp}, final score: {count_comp}")
                     print("You went over. You lose :(")
-                    choice = input("Do u want to play a game of Blackjack? Type 'y' or 'n'" )
-                    if choice == "n":
-                        game = False
-                        blackjack = False
-                    elif choice == "y":
-                        game = True
-                        count_comp = 0
-                        count_user = 0
-                        user = []
-                        comp = []
-                        print(art.logo)
-                        user.append(random.choice(cards))
-                        user.append(random.choice(cards))
-                        comp.append(random.choice(cards))
-                        for add in user:
-                            count_user += add
-
-                    else:
-                        blackjack = False
+                    game = False
+                    neustart()
             elif choice == "n":
                 while count_comp < 17:
-                    comp.append(random.choice(cards))
-                    count_comp = 0
-                    for add in comp:
-                        count_comp += add
-                    if 11 in comp and count_comp > 21:
-                        comp[comp.index(11)] = 1
-                        count_comp -= 10
-
+                    comp_add()
 
                 print(f"Your final hand: {user}, current score: {count_user}")
                 print(f"Computer's final hand: {comp}, final score: {count_comp}")
@@ -88,20 +95,12 @@ while blackjack:
         game = False
 
         if blackjack:
-            choice = input("Do u want to play a game of Blackjack? Type 'y' or 'n'")
-            if choice == "n":
-                blackjack = False
-            elif choice == "y":
-                game = True
-                count_comp = 0
-                count_user = 0
-                user = []
-                comp = []
-            else:
-                blackjack = False
+            neustart()
 
     elif choice == "n":
         blackjack = False
     else:
         choice = input("pls enter y or n ")
+
+
 
